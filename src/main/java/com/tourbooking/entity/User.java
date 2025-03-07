4960b93c2af36a6cbaab0d6e3eef6a48699db92d
Trong Tai <tai.nguyen7922@gmail.com>
2025-03-07 15:30:30 +0700

    config projects

---
+++ src/main/java/com/tourbooking/entity/User.java
@@ -0,0 +1,11 @@
+package com.tourbooking.entity;
+
+public class User {
+    private String username;
+    private String password;
+    private String role;
+    private String email;
+    private String phone;
+    private String address;
+    private String fullname;
+}
