90944810341bc1ba2456ce50df3d4e477b3b2f74
Trong Tai <tai.nguyen7922@gmail.com>
2025-03-07 14:29:28 +0700

    push project to github

---
+++ src/main/java/com/tourbooking/TourBookingApplication.java
@@ -0,0 +1,14 @@
+package com.tourbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourBookingApplication.class, args);
+        System.out.println("Tour Booking Application is running...12345");
+    }
+
+}
